64714485c4e0e2b7c92ee99924f514a39a7aa0fe
HoangTien0105 <hoangtien01052003@gmail.com>
2024-10-09 11:07:23 +0700

    update new repository

---
+++ app/src/main/java/com/example/pokemonshop/api/APIClient.java
@@ -0,0 +1,29 @@
+package com.example.pokemonshop.api;
+
+import com.example.pokemonshop.adapters.CustomDateTypeAdapter;
+import com.example.pokemonshop.adapters.CustomDateTypeAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.Date;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static String baseURL = "http://localhost:7174/api/v1/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            Gson gson = new GsonBuilder()
+                    .registerTypeAdapter(Date.class, new CustomDateTypeAdapter())
+                    .create();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+        }
+        return retrofit;
+    }
+}
