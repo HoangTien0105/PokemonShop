dd20c4c6803579fbb2317d61de84fc9158ab4339
HoangTien0105 <hoangtien01052003@gmail.com>
2024-11-09 01:10:29 +0700

    resolve conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("androidx.appcompat:appcompat:1.4.0")
     implementation ("com.github.bumptech.glide:glide:4.14.2")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
